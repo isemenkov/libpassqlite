cdff8a170f11c72c34d72cedcfccd71e4805950a
Ivan Semenkov <ivan@semenkov.pro>
2020-09-30 08:54:42 +0300

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# libPasSQLite
+libPasSQLite
+============
+It is object pascal bindings and wrapper around [SQLite library](https://www.sqlite.org). SQLite is library that implements a small, fast, self-contained, high-reliability, full-featured, SQL database engine.
+
---
+++ source/libpassqlite.pas
@@ -0,0 +1,54 @@
+(******************************************************************************)
+(*                                libPasSQLite                                *)
+(*               object pascal wrapper around SQLLite library                 *)
+(*                        https://github.com/curl/curl                        *)
+(*                                                                            *)
+(* Copyright (c) 2020                                       Ivan Semenkov     *)
+(* https://github.com/isemenkov/libpassqlite                ivan@semenkov.pro *)
+(*                                                          Ukraine           *)
+(******************************************************************************)
+(*                                                                            *)
+(* This source  is free software;  you can redistribute  it and/or modify  it *)
+(* under the terms of the GNU General Public License as published by the Free *)
+(* Software Foundation; either version 3 of the License.                      *)
+(*                                                                            *)
+(* This code is distributed in the  hope that it will  be useful, but WITHOUT *)
+(* ANY  WARRANTY;  without even  the implied  warranty of MERCHANTABILITY  or *)
+(* FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License for *)
+(* more details.                                                              *)
+(*                                                                            *)
+(* A copy  of the  GNU General Public License is available  on the World Wide *)
+(* Web at <http://www.gnu.org/copyleft/gpl.html>. You  can also obtain  it by *)
+(* writing to the Free Software Foundation, Inc., 51  Franklin Street - Fifth *)
+(* Floor, Boston, MA 02110-1335, USA.                                         *)
+(*                                                                            *)
+(******************************************************************************)
+unit libpassqlite;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses 
+  SysUtils;
+
+{$IFDEF FPC}
+  {$PACKRECORDS C}
+{$ENDIF}
+  
+const
+{$IFDEF WINDOWS}
+  sqlite3_lib = 'sqlite3.dll';
+{$ENDIF}
+{$IFDEF UNIX}
+  sqlite3_lib = 'sqlite3.so';
+{$ENDIF}
+{$IFDEF DARWIN}
+  sqlite3_lib = 'libsqlite3.dylib';
+{$ENDIF}
+
+
+
+implementation
+
+end.
