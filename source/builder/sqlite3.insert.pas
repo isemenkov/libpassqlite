48f19cb4bce9b4f690bc618f0bd1fb099e60560b
Ivan Semenkov <ivan@semenkov.pro>
2020-10-07 08:22:23 +0300

    Refactor TSQLite3Insert Update unit-tests

--- source/builder/sqlite3.insert.pas
+++ source/builder/sqlite3.insert.pas
@@ -36,59 +36,6 @@ uses
   sqlite3.query, sqlite3.structures, sqlite3.result_row;
 
 type
-  PSQLite3Insert = ^TSQLite3Insert;
-  TInsertValuesList = class;
-
-  { Insert columns list. }
-  TInsertColumnsList = class
-  public
-    constructor Create;
-    destructor Destroy; override;
-
-    function Column (AColumnName : String; AColumnType : TDataType) : 
-      TInsertColumnsList;
-    
-    { Facade for TSQLite3Insert.Values function. }
-    function Values : TInsertValuesList;
-  private
-    FColumnsList : TSQLite3Structures.TValuesList;
-    FInsert : PSQLite3Insert;
-  end;
-
-  { Insert data rows list. }
-  TInsertValuesList = class
-  public
-    constructor Create;
-    destructor Destroy; override;
-
-    { Start new insert row. }
-    function Row : TInsertValuesList;
-
-    { Add values to insert row. }
-    function Value (AValue : Integer) : TInsertValuesList; overload;
-    function Value (AValue : Double) : TInsertValuesList; overload;
-    function Value (AValue : String) : TInsertValuesList; overload;
-    function ValueNull : TInsertValuesList;
-
-    { Facade for TSQLite3Insert.Get function. }
-    function Get : Integer;
-  private
-    type
-      TValuesListCompareFunctor = class
-        (specialize TBinaryFunctor<TSQLite3Structures.TValuesList, Integer>)
-      public
-        function Call (AValue1, AValue2 : TSQLite3Structures.TValuesList) : 
-          Integer; override;
-      end;
-
-      TValuesList = class
-        (specialize TList<TSQLite3Structures.TValuesList, 
-        TValuesListCompareFunctor>);
-  private
-    FValuesList : TValuesList;
-    FInsert : PSQLite3Insert;
-  end;
-
   TSQLite3Insert = class
   public
     constructor Create (AErrorsStack : PSQL3LiteErrorsStack; ADBHandle :
@@ -96,19 +43,26 @@ type
     destructor Destroy; override;
 
     { Add value to insert list. }
-    function Value (AColumnName : String) : TSQLite3Insert; overload;
     function Value (AColumnName : String; AValue : Integer) : TSQLite3Insert; 
       overload;
     function Value (AColumnName : String; AValue : Double) : TSQLite3Insert;
       overload;
     function Value (AColumnName : String; AValue : String) : TSQLite3Insert;
       overload;
+    function ValueNull (AColumnName : String) : TSQLite3Insert; overload;
 
-    { Inserted columns list. }
-    function Columns : TInsertColumnsList;
+    { Set multiple insert column data. }
+    function Column (AColumnName : String; AColumnType : TDataType) : 
+      TSQLite3Insert;
 
-    { Inserted values list. }
-    function Values : TInsertValuesList;
+    { Start new insert row. }
+    function Row : TSQLite3Insert;
+
+    { Add values to insert row. }
+    function Value (AValue : Integer) : TSQLite3Insert; overload;
+    function Value (AValue : Double) : TSQLite3Insert; overload;
+    function Value (AValue : String) : TSQLite3Insert; overload;
+    function ValueNull : TSQLite3Insert;
 
     { Get result. }
     function Get : Integer;  
@@ -118,160 +72,34 @@ type
     function BindMultipleQueryData (AQuery : TSQLite3Query; AIndex : Integer) : 
       Integer;
   private
+    type
+      TMultipleValuesListCompareFunctor = class
+        (specialize TBinaryFunctor<TSQLite3Structures.TValuesList, Integer>)
+      public
+        function Call (AValue1, AValue2 : TSQLite3Structures.TValuesList) : 
+          Integer; override;
+      end;
+
+      TMultipleValuesList = class
+        (specialize TList<TSQLite3Structures.TValuesList, 
+        TMultipleValuesListCompareFunctor>);
+  private
     FErrorsStack : PSQL3LiteErrorsStack;
     FDBHandle : ppsqlite3;
     FTableName : String;
     FValuesList : TSQLite3Structures.TValuesList;
-    
-    FMultipleColumns : TInsertColumnsList;
-    FMultipleValues : TInsertValuesList;
+    FColumnsList : TSQLite3Structures.TValuesList;
+    FMultipleValuesList : TMultipleValuesList;
   end;
 
 implementation
 
-{ TInsertColumnsList }
+{ TSQLite3Insert.TValuesListCompareFunctor }
 
-constructor TInsertColumnsList.Create;
-begin
-  FColumnsList := TSQLite3Structures.TValuesList.Create;
-  FInsert := nil;
-end;
-
-destructor TInsertColumnsList.Destroy;
-begin
-  FreeAndNil(FColumnsList);
-  inherited Destroy;
-end;
-
-function TInsertColumnsList.Column (AColumnName : String; AColumnType : 
-  TDataType) : TInsertColumnsList;
-var
-  item : TSQLite3Structures.TValueItem;
-begin
-  item.Column_Name := AColumnName;
-  item.Value_Type := AColumnType;
-  
-  item.Value_Integer := 0;
-  item.Value_Float := 0;
-  item.Value_Text := '';
-  item.Value_Blob := nil;
-
-  FColumnsList.Append(item);
-  Result := Self;
-end;
-
-function TInsertColumnsList.Values : TInsertValuesList;
-begin
-  Result := FInsert^.Values;
-end;
-
-{ TInsertValuesList.TValuesListCompareFunctor }
-
-function TInsertValuesList.TValuesListCompareFunctor.Call (AValue1, AValue2 : 
-  TSQLite3Structures.TValuesList) : Integer;
+function TSQLite3Insert.TMultipleValuesListCompareFunctor.Call (AValue1, 
+  AValue2 : TSQLite3Structures.TValuesList) : Integer;
 begin
   Result := Longint((AValue1 <> nil) and (AValue2 <> nil));
-end;
-
-{ TInsertValuesList }
-
-constructor TInsertValuesList.Create;
-begin
-  FValuesList := TInsertValuesList.TValuesList.Create;
-  FInsert := nil;
-end;
-
-destructor TInsertValuesList.Destroy;
-begin
-  FreeAndNil(FValuesList);
-  inherited Destroy;
-end;
-
-function TInsertValuesList.Row : TInsertValuesList;
-begin
-  FValuesList.Append(TSQLite3Structures.TValuesList.Create);
-  Result := Self;
-end;
-
-function TInsertValuesList.Value (AValue : Integer) : TInsertValuesList;
-var
-  item : TSQLite3Structures.TValueItem;
-begin
-  if FValuesList.LastEntry.HasValue then
-  begin
-    item.Column_Name := '';
-    item.Value_Type := SQLITE_INTEGER;
-    item.Value_Integer := AValue;
-    item.Value_Float := 0;
-    item.Value_Text := '';
-    item.Value_Blob := nil;
-
-    FValuesList.LastEntry.Value.Append(item);
-  end;
-
-  Result := Self;
-end;
-
-function TInsertValuesList.Value (AValue : Double) : TInsertValuesList;
-var
-  item : TSQLite3Structures.TValueItem;
-begin
-  if FValuesList.LastEntry.HasValue then
-  begin
-    item.Column_Name := '';
-    item.Value_Type := SQLITE_FLOAT;
-    item.Value_Integer := 0;
-    item.Value_Float := AValue;
-    item.Value_Text := '';
-    item.Value_Blob := nil;
-
-    FValuesList.LastEntry.Value.Append(item);
-  end;
-
-  Result := Self;
-end;
-
-function TInsertValuesList.Value (AValue : String) : TInsertValuesList;
-var
-  item : TSQLite3Structures.TValueItem;
-begin
-  if FValuesList.LastEntry.HasValue then
-  begin
-    item.Column_Name := '';
-    item.Value_Type := SQLITE_TEXT;
-    item.Value_Integer := 0;
-    item.Value_Float := 0;
-    item.Value_Text := AValue;
-    item.Value_Blob := nil;
-
-    FValuesList.LastEntry.Value.Append(item);
-  end;
-
-  Result := Self;
-end;
-
-function TInsertValuesList.ValueNull : TInsertValuesList;
-var
-  item : TSQLite3Structures.TValueItem;
-begin
-  if FValuesList.LastEntry.HasValue then
-  begin
-    item.Column_Name := '';
-    item.Value_Type := SQLITE_NULL;
-    item.Value_Integer := 0;
-    item.Value_Float := 0;
-    item.Value_Text := '';
-    item.Value_Blob := nil;
-
-    FValuesList.LastEntry.Value.Append(item);
-  end;
-
-  Result := Self;
-end;
-
-function TInsertValuesList.Get : Integer;
-begin
-  Result := FInsert^.Get;
 end;
 
 { TSQLite3Insert }
@@ -283,37 +111,17 @@ begin
   FDBHandle := ADBHandle;
   FTableName := ATableName;
   FValuesList := TSQLite3Structures.TValuesList.Create;
-
-  FMultipleColumns := TInsertColumnsList.Create;
-  FMultipleValues := TInsertValuesList.Create;
-
-  FMultipleColumns.FInsert := @Self;
-  FMultipleValues.FInsert := @Self;
+  FColumnsList := TSQLite3Structures.TValuesList.Create;
+  FMultipleValuesList := TMultipleValuesList.Create;
 end;
 
 destructor TSQLite3Insert.Destroy;
 begin
   FreeAndNil(FValuesList);
-  FreeAndNil(FMultipleColumns);
-  FreeAndNil(FMultipleValues);
+  FreeAndNil(FColumnsList);
+  FreeAndNil(FMultipleValuesList);
 
   inherited Destroy;
-end;
-
-function TSQLite3Insert.Value (AColumnName : String) : TSQLite3Insert;
-var
-  val : TSQLite3Structures.TValueItem;
-begin
-  val.Column_Name := AColumnName;
-
-  val.Value_Type := SQLITE_NULL;
-  val.Value_Integer := 0;
-  val.Value_Float := 0;
-  val.Value_Text := '';
-  val.Value_Blob := nil;
-
-  FValuesList.Append(val);
-  Result := Self;
 end;
 
 function TSQLite3Insert.Value (AColumnName : String; AValue : Integer) : 
@@ -367,14 +175,119 @@ begin
   Result := Self;
 end;
 
-function TSQLite3Insert.Columns : TInsertColumnsList;
+function TSQLite3Insert.ValueNull (AColumnName : String) : TSQLite3Insert;
+var
+  val : TSQLite3Structures.TValueItem;
 begin
-  Result := FMultipleColumns;
+  val.Column_Name := AColumnName;
+
+  val.Value_Type := SQLITE_NULL;
+  val.Value_Integer := 0;
+  val.Value_Float := 0;
+  val.Value_Text := '';
+  val.Value_Blob := nil;
+
+  FValuesList.Append(val);
+  Result := Self;
 end;
 
-function TSQLite3Insert.Values : TInsertValuesList;
+function TSQLite3Insert.Column (AColumnName : String; AColumnType : 
+  TDataType) : TSQLite3Insert;
+var
+  item : TSQLite3Structures.TValueItem;
 begin
-  Result := FMultipleValues;
+  item.Column_Name := AColumnName;
+  item.Value_Type := AColumnType;
+  
+  item.Value_Integer := 0;
+  item.Value_Float := 0;
+  item.Value_Text := '';
+  item.Value_Blob := nil;
+
+  FColumnsList.Append(item);
+  Result := Self;
+end;
+
+function TSQLite3Insert.Row : TSQLite3Insert;
+begin
+  FMultipleValuesList.Append(TSQLite3Structures.TValuesList.Create);
+  Result := Self;
+end;
+
+function TSQLite3Insert.Value (AValue : Integer) : TSQLite3Insert;
+var
+  item : TSQLite3Structures.TValueItem;
+begin
+  if FMultipleValuesList.LastEntry.HasValue then
+  begin
+    item.Column_Name := '';
+    item.Value_Type := SQLITE_INTEGER;
+    item.Value_Integer := AValue;
+    item.Value_Float := 0;
+    item.Value_Text := '';
+    item.Value_Blob := nil;
+
+    FMultipleValuesList.LastEntry.Value.Append(item);
+  end;
+
+  Result := Self;
+end;
+
+function TSQLite3Insert.Value (AValue : Double) : TSQLite3Insert;
+var
+  item : TSQLite3Structures.TValueItem;
+begin
+  if FMultipleValuesList.LastEntry.HasValue then
+  begin
+    item.Column_Name := '';
+    item.Value_Type := SQLITE_FLOAT;
+    item.Value_Integer := 0;
+    item.Value_Float := AValue;
+    item.Value_Text := '';
+    item.Value_Blob := nil;
+
+    FMultipleValuesList.LastEntry.Value.Append(item);
+  end;
+
+  Result := Self;
+end;
+
+function TSQLite3Insert.Value (AValue : String) : TSQLite3Insert;
+var
+  item : TSQLite3Structures.TValueItem;
+begin
+  if FMultipleValuesList.LastEntry.HasValue then
+  begin
+    item.Column_Name := '';
+    item.Value_Type := SQLITE_TEXT;
+    item.Value_Integer := 0;
+    item.Value_Float := 0;
+    item.Value_Text := AValue;
+    item.Value_Blob := nil;
+
+    FMultipleValuesList.LastEntry.Value.Append(item);
+  end;
+
+  Result := Self;
+end;
+
+function TSQLite3Insert.ValueNull : TSQLite3Insert;
+var
+  item : TSQLite3Structures.TValueItem;
+begin
+  if FMultipleValuesList.LastEntry.HasValue then
+  begin
+    item.Column_Name := '';
+    item.Value_Type := SQLITE_NULL;
+    item.Value_Integer := 0;
+    item.Value_Float := 0;
+    item.Value_Text := '';
+    item.Value_Blob := nil;
+
+    FMultipleValuesList.LastEntry.Value.Append(item);
+  end;
+
+  Result := Self;
 end;
 
 function TSQLite3Insert.Get : Integer;
@@ -459,19 +372,19 @@ var
   value_item : TSQLite3Structures.TValueItem;
   i, j : Integer;
 begin
-  if (not FMultipleColumns.FColumnsList.FirstEntry.HasValue) or
-     (not FMultipleValues.FValuesList.FirstEntry.HasValue) then
+  if (not FColumnsList.FirstEntry.HasValue) or
+     (not FMultipleValuesList.FirstEntry.HasValue) then
     Exit('');
   
   SQL := '';
 
   { If columns list is not empty. }
-  if FMultipleColumns.FColumnsList.FirstEntry.HasValue then
+  if FColumnsList.FirstEntry.HasValue then
   begin
     SQL := SQL + 'INSERT INTO ' + FTableName + ' (';
 
     i := 0;
-    for column_item in FMultipleColumns.FColumnsList do
+    for column_item in FColumnsList do
     begin
       { For each column name. }
       if i > 0 then
@@ -488,9 +401,9 @@ begin
 
   { For each row in list. }
   i := 0;
-  for value_row in FMultipleValues.FValuesList do
+  for value_row in FMultipleValuesList do
   begin
-    column_iterator := FMultipleColumns.FColumnsList.FirstEntry;
+    column_iterator := FColumnsList.FirstEntry;
     if i > 0 then
       SQL := SQL + ',';
 
@@ -525,13 +438,13 @@ var
   value_item : TSQLite3Structures.TValueItem;
   i : Integer;
 begin
-  if not FMultipleValues.FValuesList.FirstEntry.HasValue then
+  if not FMultipleValuesList.FirstEntry.HasValue then
     Exit(AIndex);
 
   i := AIndex;
 
   { For each row in list. }
-  for value_row in FMultipleValues.FValuesList do
+  for value_row in FMultipleValuesList do
   begin
     for value_item in value_row do
     begin
--- unit-tests/builder_testcase.pas
+++ unit-tests/builder_testcase.pas
@@ -152,35 +152,67 @@ begin
 
   inserted_rows := 0;
   inserted_rows := builder.Table('test_table').Insert
-    .Columns
-      .Column('val_1', SQLITE_INTEGER)
-      .Column('val_2', SQLITE_FLOAT)
-      .Column('val_3', SQLITE_TEXT)
-    .Values
-      .Row
-        .Value(12)
-        .Value(3.14)
-        .Value('some value')
-      .Row
-        .Value(54)
-        .Value(6.54)
-        .Value('string value')
-      .Row
-        .Value(-874)
-        .Value(532.00)
-        .Value('test value')
+    .Column('val_1', SQLITE_INTEGER)
+    .Column('val_2', SQLITE_FLOAT)
+    .Column('val_3', SQLITE_TEXT)
+    .Row
+      .Value(12)
+      .Value(3.14)
+      .Value('some value')
+    .Row
+      .Value(54)
+      .Value(6.54)
+      .Value('string value')
+    .Row
+      .Value(-874)
+      .Value(532.00)
+      .Value('test value')
     .Get;
 
   AssertTrue('Database inserted rows count is not correct', inserted_rows = 3);
 
+  counter := 0;
+  for row in builder.Table('test_table').Select.All.Get do
+  begin
+    case counter of
+      0 : begin
+        AssertTrue('Selected row ''val_1'' column value is not correct', 
+          row.GetIntegerValue('val_1') = 12);
+        AssertEquals('Selected row ''val_2'' column value is not correct',
+          row.GetDoubleValue('val_2'), 3.14, 0.01);
+        AssertTrue('Selected row ''val_3'' column value is not correct', 
+          row.GetStringValue('val_3') = 'some value');
+      end;
+      1 : begin
+        AssertTrue('Selected row ''val_1'' column value is not correct', 
+          row.GetIntegerValue('val_1') = 54);
+        AssertEquals('Selected row ''val_2'' column value is not correct',
+          row.GetDoubleValue('val_2'), 6.54, 0.01);
+        AssertTrue('Selected row ''val_3'' column value is not correct', 
+          row.GetStringValue('val_3') = 'string value');
+      end;
+      2 : begin
+        AssertTrue('Selected row ''val_1'' column value is not correct', 
+          row.GetIntegerValue('val_1') = -874);
+        AssertEquals('Selected row ''val_2'' column value is not correct',
+          row.GetDoubleValue('val_2'), 532.00, 0.01);
+        AssertTrue('Selected row ''val_3'' column value is not correct', 
+          row.GetStringValue('val_3') = 'test value');
+      end;
+      3 : begin
+        Fail('Impossible row.');
+      end;
+    end;
+    Inc(counter);
+  end;
 
+  AssertTrue('Database selected rows count is not correct', counter = 3);
 
   FreeAndNil(builder);
 
   AssertTrue('Database file not exists', FileExists('test.db'));
 
   DeleteFile('test.db');
-
 end;
 
 procedure TSQLite3BuilderTestCase.Test_SQLite3Builder_CheckTableSchema;
